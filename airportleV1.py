2b0e186e97a697ea35dfe2d7d648cbbf868c3db4
wrmf <31448920+wrmf@users.noreply.github.com>
2022-02-27 09:39:13 -0600

    Added comments

--- airportleV1.py
+++ airportleV1.py
@@ -2,12 +2,14 @@ import numpy as np
 import pandas as pd
 import xarray as xr
 
+#Check if char is valid within the game (letter or ?)
 def isValidChar(input: str):
     if input[0].isalpha() or input[0] == '?':
         return True
     else:
         return False
 
+#Check if code is valid (letters or ?)
 def isValidCode(input: str):
     if len(input) > 3:
         return False
@@ -16,61 +18,64 @@ def isValidCode(input: str):
     else:
         return True
 
+#Check if code is "???" because you are helpless if so
 def isHelpless(input: str):
     if input[0] == '?' and input[1] == '?' and input[2] == '?':
         return True
     else:
         return False
 
+#Search array on one char
 def searchOnChar(input, charToSearch: str, searchCharNum: int):
 
+    #Length wrapping
     if searchCharNum < 0:
         searchCharNum = 0
     elif searchCharNum > 2:
         searchCharNum = 2
 
-    numID = 0
+    numID = 0 #Counter
 
+    #Check array
     for n in input["Code"]:
-        if not len(n) > 0:
-            pass
-        elif n[searchCharNum] != charToSearch:
+        if n[searchCharNum] != charToSearch:
             input = input.drop([numID])
         numID += 1
 
-    input = input.reset_index(drop=True)
+    input = input.reset_index(drop=True) #Reset length because .drop is strange
 
     return input
 
-
-
-
-
-filenames = ["airportCodes.txt"]
-columns = ["City", "Code"]
+filenames = ["airportCodes.txt"] #File names to read in
+columns = ["City", "Code"] #Columns for pandas array
 
 print("Reading in files...")
 
+#Read files
 for filename in filenames:
     dataframe = pd.read_csv(filename, header = None, delimiter="(", names=columns)
 
-dataframe["Code"] = dataframe["Code"].str[:-1]
-dataframe.sort_values("Code")
+dataframe["Code"] = dataframe["Code"].str[:-1] #Delete ) from end of string
+dataframe.sort_values("Code") #Sort values by code... does this do anything?
 
 print("Welcome to Airportle Solver")
 print("by William McGovern-Fagg")
 print("Type 'exit' at any time to exit")
 print("Read in file")
 
-isDone = False
+isDone = False #Flag for done
 
+#Repeat until exited
 while(not isDone):
-    code = input("Please input the current airport code:").upper()
+
+    code = input("Please input the current airport code:").upper() #Get input
+
     if code == 'EXIT':
+        print("Exiting!")
         isDone = True
 
     #Check for validity of airport code
-    if not isValidCode(code):
+    elif not isValidCode(code):
         print(f"{code} is not a valid airport code, please try again")
     #Check if airport code has one letter filled in
     elif isHelpless(code):
@@ -86,6 +91,8 @@ while(not isDone):
             tempDataFrame = searchOnChar(tempDataFrame, code[2], 2)
 
         index = 0
+        print("\n******************************************\n")
         while index < len(tempDataFrame["Code"]):
             print(f"{tempDataFrame['Code'][index]}: {tempDataFrame['City'][index]}")
             index += 1
+        print("\n******************************************\n")
